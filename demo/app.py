cfaa285297c37a7c2a4cfb3f722faf786b1563e6
Piyush Bagad <piyushnbagad11@gmail.com>
2024-11-17 17:00:28 +0000

    Almost ready demo

--- demo/app.py
+++ demo/app.py
@@ -1,12 +1,30 @@
 import os
+import sys
+sys.path.append("../")
+
 import gradio as gr
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams["font.family"] = "serif"
+import decord
+import PIL, PIL.Image
+import librosa
+from IPython.display import Markdown, display
+import pandas as pd
 
 from demo.util import (
 	custom_css,
 	custom_html,
 	read_html_file,
 )
+import shared.utils as su
+import sound_of_water.audio_pitch.model as audio_models
+import sound_of_water.data.audio_loader as audio_loader
+import sound_of_water.data.audio_transforms as at
+import sound_of_water.data.csv_loader as csv_loader
 
+from demo.util import *
 
 
 css = """
@@ -15,12 +33,14 @@ css = """
         font-family: 'Arial', serif;
         margin: 0;
         padding: 0;
+        color: black;
     }
     .header {
         display: flex;
         align-items: center;
         justify-content: center;
         margin-top: 5px;
+        color: black;
     }
     .footer {
         display: flex;
@@ -33,6 +53,7 @@ css = """
     }
     .content {
         text-align: center;
+        color: black;
     }
     .title {
         font-size: 2.5em;
@@ -55,7 +76,6 @@ css = """
     }
     .button {
         background-color: #545758;
-        color: white;
         text-decoration: none;
         padding: 8px 16px;
         border-radius: 5px;
@@ -76,12 +96,12 @@ header = css + """
     <div class="content">
         <img src="https://bpiyush.github.io/pouring-water-website/assets/pouring-water-logo5.png" alt="logo" width="80" style="margin-bottom: -50px; margin-right: 30px;">
         <div class="title" style="font-size: 44px; margin-left: -30px;">The Sound of Water</div>
-        <div class="subtitle" style="font-size: 30px; margin-left: -30px;"><b>Inferring Physical Properties from Pouring Liquids</b></div>
+        <div style="font-size: 30px; margin-left: -30px;"><b>Inferring Physical Properties from Pouring Liquids</b></div>
         <div class="authors">
-            <a href="https://bpiyush.github.io/">Piyush Bagad</a><sup>1</sup>,
-            <a href="https://makarandtapaswi.github.io/">Makarand Tapaswi</a><sup>2</sup>,
-            <a href="https://www.ceessnoek.info/">Cees G. M. Snoek</a><sup>3</sup>,
-            <a href="https://www.robots.ox.ac.uk/~az/">Andrew Zisserman</a><sup>1</sup>,
+            <a style="color: #92eaff; href="https://bpiyush.github.io/">Piyush Bagad</a><sup>1</sup>,
+            <a style="color: #92eaff; href="https://makarandtapaswi.github.io/">Makarand Tapaswi</a><sup>2</sup>,
+            <a style="color: #92eaff; href="https://www.ceessnoek.info/">Cees G. M. Snoek</a><sup>3</sup>,
+            <a style="color: #92eaff; href="https://www.robots.ox.ac.uk/~az/">Andrew Zisserman</a><sup>1</sup>,
         </div>
         <div class="affiliations">
             <sup>1</sup>University of Oxford, <sup>2</sup>IIIT Hyderabad, <sup>3</sup>University of Amsterdam
@@ -100,18 +120,82 @@ header = css + """
 """
 
 footer = css + """
-<div class="header">
+<div class="header" style="justify-content: left;">
 <div class="content" style="font-size: 16px;">
 Please give us a 🌟 on <a href='https://github.com/bpiyush/SoundOfWater'>Github</a> if you like our work!
 Tips to get better results:
 <br><br>
-<ul style="text-align: left; margin: -5px; padding: -5px">
+<ol style="text-align: left; font-size: 14px; margin-left: 30px">
     <li>Make sure there is not too much noise such that the pouring is audible.</li>
-    <li>The video is not used during the inference.</li>
-</ul>
+    <li>Note that the video is not used during the inference. Only the audio must be clear enough.</li>
+</ol>
 </div>
 </div>
 """
+
+# def process_input(video=None, youtube_link=None, start_time=None, end_time=None):
+#     if video:
+#         return f"Video file uploaded: {video.name}"
+#     elif youtube_link and start_time and end_time:
+#         return f"YouTube link: {youtube_link} (Start: {start_time}, End: {end_time})"
+#     else:
+#         return "Please upload a video or provide a YouTube link with start and end times."
+
+
+def configure_input():
+    gr.Markdown(
+        "#### Either upload a video file or provide a YouTube link with start and end times."
+    )
+    video_input = gr.Video(label="Upload Video", height=480)
+    youtube_link_start = gr.Textbox(label="YouTube Link (Start time)")
+    youtube_link_end = gr.Textbox(label="YouTube Link (End time)")
+    return [video_input, youtube_link_start, youtube_link_end]
+
+
+# Example usage in a Gradio interface
+def process_input(video, youtube_link_start, youtube_link_end):
+    if video is not None:
+        print(video)
+
+        # Load model globally
+        model = load_model()
+
+        # The input is a video file path
+        video_path = video
+
+        # Load first frame
+        frame = load_frame(video_path)
+
+        # Load spectrogram
+        S = load_spectrogram(video_path)
+
+        # Load audio tensor
+        audio = load_audio_tensor(video_path)
+
+        # Get output
+        z_audio, y_audio = get_model_output(audio, model)
+
+        # Show image output
+        image, df_show, tsne_image = show_output(frame, S, y_audio, z_audio)
+
+        return image, df_show, gr.Markdown(note), tsne_image
+
+    elif (youtube_link_start is not None) and (youtube_link_end is not None):
+        # Using the provided YouTube link
+        # Example: https://youtu.be/6-HVn8Jzzuk?t=10
+        start_link = f"Processing YouTube link: {youtube_link_start}"
+        end_link = f"Processing YouTube link: {youtube_link_end}"
+
+        # Get video ID
+        video_id = youtube_link_start.split("/")[-1].split("?")[0]
+        assert video_id == youtube_link_end.split("/")[-1].split("?")[0], "Video IDs do not match"
+        start_time = float(youtube_link_start.split("t=")[-1])
+        end_time = float(youtube_link_end.split("t=")[-1])
+
+        raise NotImplementedError("YouTube link processing is not implemented yet")
+    else:
+        return "No input provided"
+
 
 def greet(name, is_morning, temperature):
     salutation = "Good morning" if is_morning else "Good evening"
@@ -120,21 +204,38 @@ def greet(name, is_morning, temperature):
     return greeting, round(celsius, 2)
 
 
+
+note = """
+**Note**: Radius (as well as height) estimation depends on accurate wavelength estimation towards the end.
+Thus, it may not be accurate if the wavelength is not estimated correctly at the end.
+
+$$
+H = l(0) = \\frac{\lambda(0) - \lambda(T)}{4} \ \ \\text{and} \ \ R = \\frac{\lambda(T)}{4\\beta}
+$$
+"""
+
+
+def configure_outputs():
+    image_wide = gr.Image(label="Estimated pitch")
+    dataframe = gr.DataFrame(label="Estimated physical properties")
+    image_tsne = gr.Image(label="TSNE of features", width=300)
+    markdown = gr.Markdown(label="Note")
+    # ["image", "dataframe", "image", "markdown"]
+    return [image_wide, dataframe, markdown, image_tsne]
+
+
 # Define Gradio interface
 with gr.Blocks(
     css=custom_css,
-    theme=gr.themes.Default(
-        primary_hue=gr.themes.colors.red,
-        secondary_hue=gr.themes.colors.pink,
-    ),
+    theme=gr.themes.Default(),
 ) as demo:
     # Add the header
     gr.HTML(header)
-
+    
     gr.Interface(
-        fn=greet,
-        inputs=["text", "checkbox", gr.Slider(0, 100)],
-        outputs=["text", "number"],
+        fn=process_input,
+        inputs=configure_input(),
+        outputs=configure_outputs(),
     )
         
     # Add the footer
--- demo/util.py
+++ demo/util.py
@@ -1,17 +1,11 @@
 custom_css = """
 <style>
-    body {
-        background-color: #ffffff;
-        color: #333333;  /* Default text color */
-    }
     .container {
         max-width: 100% !important;
         padding-left: 0 !important;
         padding-right: 0 !important;
     }
     .header {
-        background-color: #f0f0f0;
-        color: #333333;
         padding: 30px;
         margin-bottom: 30px;
         text-align: center;
@@ -97,7 +91,306 @@ Tips to get better results:
 </div>
 """
 
+import os
+import sys
+
+import gradio as gr
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+plt.rcParams["font.family"] = "serif"
+import decord
+import PIL, PIL.Image
+import librosa
+from IPython.display import Markdown, display
+import pandas as pd
+
+from demo.util import (
+	custom_css,
+	custom_html,
+	read_html_file,
+)
+import shared.utils as su
+import sound_of_water.audio_pitch.model as audio_models
+import sound_of_water.data.audio_loader as audio_loader
+import sound_of_water.data.audio_transforms as at
+import sound_of_water.data.csv_loader as csv_loader
+
 
 def read_html_file(file):
     with open(file) as f:
         return f.read()
+
+
+
+def define_axes(figsize=(13, 4), width_ratios=[0.22, 0.78]):
+    fig, axes = plt.subplots(
+        1, 2, figsize=figsize, width_ratios=width_ratios,
+        layout="constrained",
+    )
+    return fig, axes
+
+
+def show_frame_and_spectrogram(frame, spectrogram, visualise_args, axes=None):
+    """Shows the frame and spectrogram side by side."""
+
+    if axes is None:
+        fig, axes = define_axes()
+    else:
+        assert len(axes) == 2
+
+    ax = axes[0]
+    ax.imshow(frame, aspect="auto")
+    ax.set_title("Example frame")
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax = axes[1]
+    audio_loader.show_logmelspectrogram(
+        S=spectrogram,
+        ax=ax,
+        show=False,
+        sr=visualise_args["sr"],
+        n_fft=visualise_args["n_fft"],
+        hop_length=visualise_args["hop_length"],
+    )
+
+
+def scatter_pitch(ax, t, f, s=60, marker="o", color="limegreen", label="Pitch"):
+    """Scatter plot of pitch."""
+    ax.scatter(t, f, color=color, label=label, s=s, marker=marker)
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Frequency (Hz)")
+    ax.legend(loc="upper left")
+
+
+# Load video frame
+def load_frame(video_path):
+    vr = decord.VideoReader(video_path, num_threads=1)
+    frame = PIL.Image.fromarray(vr[0].asnumpy())
+    frame = audio_loader.crop_or_pad_to_size(frame, size=(270, 480))
+    return frame
+
+
+def load_spectrogram(video_path):
+    y = audio_loader.load_audio_clips(
+        audio_path=video_path,
+        clips=None,
+        load_entire=True,
+        cut_to_clip_len=False,
+        **aload_args,
+    )[0]
+    S = audio_loader.librosa_harmonic_spectrogram_db(
+        y,
+        sr=visualise_args["sr"],
+        n_fft=visualise_args["n_fft"],
+        hop_length=visualise_args["hop_length"],
+        n_mels=visualise_args['n_mels'],
+    )
+    return S
+
+
+# Load audio
+visualise_args = {
+    "sr": 16000,
+    "n_fft": 400,
+    "hop_length": 320,
+    "n_mels": 64,
+    "margin": 16.,
+    "C": 340 * 100.,
+    "audio_output_fps": 49.,
+    "w_max": 100.,
+    "n_bins": 64,
+}
+aload_args = {
+    "sr": 16000,
+    "clip_len": None,
+    "backend": "decord",
+}
+
+
+cfg_backbone = {
+    "name": "Wav2Vec2WithTimeEncoding",
+    "args": dict(),
+}
+backbone = getattr(audio_models, cfg_backbone["name"])(
+    **cfg_backbone["args"],
+)
+
+
+cfg_model = {
+    "name": "WavelengthWithTime",
+    "args": {
+        "axial": True,
+        "axial_bins": 64,
+        "radial": True,
+        "radial_bins": 64,
+        "freeze_backbone": True,
+        "train_backbone_modules": [6, 7, 8, 9, 10, 11],
+        "act": "softmax",
+        "criterion": "kl_div",
+    }
+}
+
+
+def load_model():
+    model = getattr(audio_models, cfg_model["name"])(
+        backbone=backbone, **cfg_model["args"],
+    )
+    su.misc.num_params(model)
+
+
+    # Load the model weights from trained checkpoint
+    # NOTE: Be sure to set the correct path to the checkpoint
+    su.log.print_update("[:::] Loading checkpoint ", color="cyan", fillchar=".")
+    ckpt_dir = "/work/piyush/pretrained_checkpoints/SoundOfWater"
+    ckpt_path = os.path.join(
+        ckpt_dir, 
+        "dsr9mf13_ep100_step12423_real_finetuned_with_cosupervision.pth",
+    )
+    assert os.path.exists(ckpt_path), \
+        f"Checkpoint not found at {ckpt_path}."
+    print("Loading checkpoint from: ", ckpt_path)
+    ckpt = torch.load(ckpt_path, map_location="cpu")
+    msg = model.load_state_dict(ckpt)
+    print(msg)
+    return model
+
+
+# Define audio transforms
+cfg_transform = {
+    "audio": {
+        "wave": [
+            {
+                "name": "AddNoise",
+                "args": {
+                "noise_level": 0.001
+                },
+                "augmentation": True,
+            },
+            {
+                "name": "ChangeVolume",
+                "args": {
+                "volume_factor": [0.8, 1.2]
+                },
+                "augmentation": True,
+            },
+            {
+                "name": "Wav2Vec2WaveformProcessor",
+                "args": {
+                "model_name": "facebook/wav2vec2-base-960h",
+                "sr": 16000
+                }
+            }
+        ],
+        "spec": None,
+    }
+}
+audio_transform = at.define_audio_transforms(
+    cfg_transform, augment=False,
+)
+
+# Define audio pipeline arguments
+apipe_args = {
+    "spec_args": None,
+    "stack": True,
+}
+
+
+def load_audio_tensor(video_path):
+    # Load and transform input audio
+    audio = audio_loader.load_and_process_audio(
+        audio_path=video_path,
+        clips=None,
+        load_entire=True,
+        cut_to_clip_len=False,
+        audio_transform=audio_transform,
+        aload_args=aload_args,
+        apipe_args=apipe_args,
+    )[0]
+    return audio
+
+
+def get_model_output(audio, model):
+    with torch.no_grad():
+        NS = audio.shape[-1]
+        duration = NS / 16000
+        t = torch.tensor([[0, duration]]).unsqueeze(0)
+        x = audio.unsqueeze(0)
+        z_audio = model.backbone(x, t)[0][0].cpu()
+        y_audio = model(x, t)["axial"][0][0].cpu()
+    return z_audio, y_audio
+
+
+def show_output(frame, S, y_audio, z_audio):
+    # duration = S.shape[-1] / visualise_args["sr"]
+    # print(S.shape, y_audio.shape, z_audio.shape)
+    duration = librosa.get_duration(
+        S=S,
+        sr=visualise_args["sr"],
+        n_fft=visualise_args["n_fft"],
+        hop_length=visualise_args["hop_length"],
+    )
+    timestamps = np.linspace(0., duration, 25)
+
+    # Get timestamps at evaluation frames
+    n_frames = len(y_audio)
+    timestamps_eval = librosa.frames_to_time(
+        np.arange(n_frames),
+        sr=visualise_args['sr'],
+        n_fft=visualise_args['n_fft'],
+        hop_length=visualise_args['hop_length'],
+    )
+    # Get predicted frequencies at these times
+    wavelengths = y_audio @ torch.linspace(
+        0, visualise_args['w_max'], visualise_args['n_bins'],
+    )
+    f_pred = visualise_args['C'] / wavelengths
+    # Pick only those timestamps where we define the true pitch
+    indices = su.misc.find_nearest_indices(timestamps_eval, timestamps)
+    f_pred = f_pred[indices]
+
+    # print(timestamps, f_pred)
+
+    # Show the true/pref pitch overlaid on the spectrogram
+    fig, axes = define_axes()
+    show_frame_and_spectrogram(frame, S, visualise_args, axes=axes)
+    scatter_pitch(axes[1], timestamps, f_pred, color="white", label="Estimated pitch", marker="o", s=70)
+    axes[1].set_title("True and predicted pitch overlaid on the spectrogram")
+    # plt.show()
+    # Convert to PIL Image and return the Image
+    from PIL import Image
+
+    # Draw the figure to a canvas
+    canvas = fig.canvas
+    canvas.draw()
+
+    # Get the RGBA buffer from the figure
+    w, h = fig.canvas.get_width_height()
+    buf = canvas.tostring_rgb()
+
+    # Create a PIL image from the RGB data
+    image = Image.frombytes("RGB", (w, h), buf)
+
+
+    # Get physical properties
+    l_pred = su.physics.estimate_length_of_air_column(wavelengths)
+    l_pred_mean = l_pred.mean().item()
+    l_pred_mean = np.round(l_pred_mean, 2)
+    H_pred = su.physics.estimate_cylinder_height(wavelengths)
+    H_pred = np.round(H_pred, 2)
+    R_pred = su.physics.estimate_cylinder_radius(wavelengths)
+    R_pred = np.round(R_pred, 2)
+    # print(f"Estimated length: {l_pred_mean} cm, Estimated height: {H_pred} cm, Estimated radius: {R_pred} cm")
+    df_show = pd.DataFrame({
+        "Physical Property": ["Container height", "Container radius", "Length of air column (mean)"],
+        "Estimated Value (in cms)": [H_pred, R_pred, l_pred_mean],
+    })
+
+
+    tsne_image = su.visualize.show_temporal_tsne(
+        z_audio.detach().numpy(), timestamps_eval, show=False,
+        figsize=(6, 5), title="Temporal t-SNE of latent features",
+        return_as_pil = True,
+    )
+
+    return image, df_show, tsne_image
